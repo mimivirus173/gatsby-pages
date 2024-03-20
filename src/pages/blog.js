4347799a0cc857ca7a1f4c7c8b93c439b00115fe
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2024-03-20 10:44:24 +0000

    blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,34 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="Blogs">
+            <ul>
+            {
+                data.allFile.nodes.map(node =>(
+                    <li key={node.name}>
+                        {node.name}
+                    </li>
+                ))
+            }
+            </ul>
             <p>Blogs go here</p>
         </Layout>
     )
 }
+
+export const query = graphql`
+    query {
+        allFile {
+            nodes {
+                name
+            }
+        }
+    }
+`
 
 export const Head = () => <Seo title="Blogs" />
 
