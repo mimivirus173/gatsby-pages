aaf42ca98a287de2d08872674a2614e82767a59b
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2024-04-15 14:09:50 +0100

    bruh

---
+++ src/pages/blog/index.js
@@ -0,0 +1,37 @@
+import * as React from 'react'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
+
+const BlogPage = ({ data }) => {
+    return (
+      <Layout pageTitle="My Blog Posts">
+        {
+          data.allMdx.nodes.map((node) => (
+            <article key={node.id}>
+              <h2>{node.frontmatter.title}</h2>
+              <p>Posted: {node.frontmatter.date}</p>
+            </article>
+          ))
+        }
+      </Layout>
+    )
+  }
+  
+  export const query = graphql`
+    query {
+      allMdx(sort: { frontmatter: { date: DESC }}) {
+        nodes {
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+          }
+          id
+          excerpt
+        }
+      }
+    }
+  `
+  
+  export const Head = () => <Seo title="My Blog Posts" />
+  
+  export default BlogPage
